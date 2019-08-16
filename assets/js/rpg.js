34144b02f8a267461c667eacc8ded5d85169083d
Stephen Fox <stephen@capablecreative.com>
2019-08-16 13:10:36 -0500

    Opponenet UI connected; Next, I need to compare values and display result of the first round of combat.

--- assets/js/rpg.js
+++ assets/js/rpg.js
@@ -27,21 +27,9 @@ const raphy = {
     counter: 25,
     attack: 66
 }
-
+// -- PLAYER CHOICES ------------------ //
 $("#wolverine").click(function() {   
     stats(wolvy,"spidyPlay","caseyPlay","raphyPlay","wolvyPlay");
-
- /*   if (element.classList.contains('spidyPlay')) {
-    element.classList.remove('spidyPlay');
-    }
-    else if (element.classList.contains('caseyPlay')) {
-    element.classList.remove('caseyPlay');
-    }
-    else if (element.classList.contains('raphyPlay')) {
-    element.classList.remove('raphyPlay');
-    }
-    else (element.classList.add('wolvyPlay')); 
-    */
 });
 
 $("#caseyJones").click(function() {
@@ -51,13 +39,30 @@ $("#caseyJones").click(function() {
 
  $("#spiderMan").click(function() {
     stats(spidy,"wolvyPlay","caseyPlay","raphyPlay","spidyPlay");
-    
  });
 
 $("#raphael").click(function() {
-    stats(raphy,"wolvyPlay","caseyPlay","spidyPlay","raphyPlay");
-    
+    stats(raphy,"wolvyPlay","caseyPlay","spidyPlay","raphyPlay");  
 });
+
+// -- OPPONENT CHOICES -------------- //
+$("#wolverineOpp").click(function() {   
+    statsOpp(wolvy,"spidyPlay","caseyPlay","raphyPlay","wolvyPlay");
+});
+
+$("#caseyJonesOpp").click(function() {
+    statsOpp(casey,"spidyPlay","wolvyPlay","raphyPlay","caseyPlay");
+   
+ });
+
+ $("#spiderManOpp").click(function() {
+    statsOpp(spidy,"wolvyPlay","caseyPlay","raphyPlay","spidyPlay");
+ });
+
+$("#raphaelOpp").click(function() {
+    statsOpp(raphy,"wolvyPlay","caseyPlay","spidyPlay","raphyPlay");  
+});
+
 
 /* ==================================
 --------- argument assignments ------
@@ -73,11 +78,21 @@ function stats(w, x, y, z, a) {
     var element = document.getElementById('playerArena');
     element.classList.add(a);
 
-    var statDiv = document.createElement('DIV');
-    var insertStat = document.getElementById('playerArena');
+    var statDiv = document.createElement('UL');
+    var ulLi = document.createElement('LI');
+    var ulLi2 = document.createElement('LI');
+    var ulLi3 = document.createElement('LI');
+    var insertStat = document.getElementById('playerStats');
     var statText = document.createTextNode(w.name);
+    var statHP = document.createTextNode("HEALTH: " + w.health);
+    var statAT = document.createTextNode("ATTACK MAX: " + w.attack);
     insertStat.appendChild(statDiv);
-    statDiv.appendChild(statText); 
+    statDiv.appendChild(ulLi);
+    statDiv.appendChild(ulLi2);
+    statDiv.appendChild(ulLi3);
+    ulLi.appendChild(statText);
+    ulLi2.appendChild(statHP);
+    ulLi3.appendChild(statAT);
 
     if (element.classList.contains(x)){
     element.classList.remove(x);
@@ -89,5 +104,63 @@ function stats(w, x, y, z, a) {
     element.classList.remove(z);
     }
     document.getElementById("roster-1").style.height = "20px";
-    document.getElementById("roster-1").style.opacity = "0.0";
+    document.getElementById("roster-1").style.opacity = "0.0"; 
+    document.getElementById("roster-2").style.height = "0px";
+    document.getElementById("roster-2").style.opacity = "1.0";
 }
+function statsOpp(w, x, y, z, a) {
+    var element = document.getElementById('opponentArena');
+    element.classList.add(a);
+
+    var statDiv = document.createElement('UL');
+    var ulLi = document.createElement('LI');
+    var ulLi2 = document.createElement('LI');
+    var ulLi3 = document.createElement('LI');
+    var insertStat = document.getElementById('opponentStats');
+    var statText = document.createTextNode(w.name);
+    var statHP = document.createTextNode("HEALTH: " + w.health);
+    var statAT = document.createTextNode("ATTACK MAX: " + w.attack);
+    insertStat.appendChild(statDiv);
+    statDiv.appendChild(ulLi);
+    statDiv.appendChild(ulLi2);
+    statDiv.appendChild(ulLi3);
+    ulLi.appendChild(statText);
+    ulLi2.appendChild(statHP);
+    ulLi3.appendChild(statAT);
+
+    if (element.classList.contains(x)){
+    element.classList.remove(x);
+    }
+    else if (element.classList.contains(y)) {
+    element.classList.remove(y);
+    }
+    else if (element.classList.contains(z)) {
+    element.classList.remove(z);
+    }
+    document.getElementById("roster-2").style.height = "20px";
+    document.getElementById("roster-2").style.opacity = "0.0"; 
+}
+
+/* 
+
+--------- NON-CRITICAL ERRORS ----------------------------------
+
+    - REMOVE DIV WRAPPER AROUND LI ITEMS IN PLAYER STATS
+    - REMOVE REDUNDANT ELEMENTS FROM STATS & STATSOPP FUNCTIONS
+
+--------- SCRAPPED CODE ----------------------------------------
+    // --- original player choice if-statement -- //
+
+    if (element.classList.contains('spidyPlay')) {
+    element.classList.remove('spidyPlay');
+    }
+    else if (element.classList.contains('caseyPlay')) {
+    element.classList.remove('caseyPlay');
+    }
+    else if (element.classList.contains('raphyPlay')) {
+    element.classList.remove('raphyPlay');
+    }
+    else (element.classList.add('wolvyPlay')); 
+    
+
+*/
